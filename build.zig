2073404c21d846d213d76cbb52c881f841b500e0
Hollin Wilkins <hollinrwilkins@gmail.com>
2024-06-28 21:42:13 -0700

    Small project showing issue.

---
+++ build.zig
@@ -0,0 +1,54 @@
+const std = @import("std");
+
+// Although this function looks imperative, note that its job is to
+// declaratively construct a build graph that will be executed by an external
+// runner.
+pub fn build(b: *std.Build) void {
+    const exe = b.addStaticLibrary(.{
+        .name = "kernel",
+        .root_source_file = b.path("kernel.zig"),
+        .target = b.resolveTargetQuery(std.Target.Query{
+            .cpu_arch = .spirv64,
+            .os_tag = .vulkan,
+            .cpu_features_add = std.Target.spirv.featureSet(&.{
+                .Int64,
+                .Int16,
+                .Int8,
+                .Float64,
+                .Float16,
+                .Vector16,
+            }),
+        }),
+        .optimize = b.standardOptimizeOption(.{}),
+        .use_llvm = false,
+        .use_lld = false,
+    });
+
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
+    // This *creates* a Run step in the build graph, to be executed when another
+    // step is evaluated that depends on it. The next line below will establish
+    // such a dependency.
+    const run_cmd = b.addRunArtifact(exe);
+
+    // By making the run step depend on the install step, it will be run from the
+    // installation directory rather than directly from within the cache directory.
+    // This is not necessary, however, if the application depends on other installed
+    // files, this ensures they will be present and in the expected location.
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    // This allows the user to pass arguments to the application in the build
+    // command itself, like this: `zig build run -- arg1 arg2 etc`
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+
+    // This creates a build step. It will be visible in the `zig build --help` menu,
+    // and can be selected like this: `zig build run`
+    // This will evaluate the `run` step rather than the default, which is "install".
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
